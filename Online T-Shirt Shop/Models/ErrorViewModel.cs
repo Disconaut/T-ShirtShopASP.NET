9d1fb04d91439b62bd908e54702240e28a3d758a
Disconaut <omelanchuk98@gmail.com>
2020-02-25 01:29:14 +0200

    Add project files.

---
+++ Online T-Shirt Shop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Online_T_Shirt_Shop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
