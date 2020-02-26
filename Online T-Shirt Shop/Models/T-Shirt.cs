8160032d8ae4e7cc044770b20983e93ebe5578f8
Disconaut <omelanchuk98@gmail.com>
2020-02-26 02:26:22 +0200

    Slightly stylized home page

---
+++ Online T-Shirt Shop/Models/T-Shirt.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_T_Shirt_Shop.Models
+{
+    public class T_Shirt
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int InStock { get; set; }
+    }
+}
