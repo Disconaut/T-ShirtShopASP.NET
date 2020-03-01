3e1698a36cfb19965e90a86cdf4d05741bcfeebd
kdanieldk <vitaliyvytrykush2001@gmail.com>
2020-03-02 00:24:37 +0200

    okey

--- Online T-Shirt Shop/Controllers/HomeController.cs
+++ Online T-Shirt Shop/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace Online_T_Shirt_Shop.Controllers
             return View();
         }
 
+        public RedirectResult SomeMethod()
+        {
+            return RedirectPermanent("/Home/RegistrationForm");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
