5944a3f1a050b678384758d1d0cb0a8c4adab49f
Disconaut <omelanchuk98@gmail.com>
2020-03-05 00:40:20 +0200

    Edited consumer

---
+++ Online T-Shirt Shop/Areas/Identity/Data/Enums/Genders.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Online_T_Shirt_Shop.Areas.Identity.Data.Enums
+{
+    public enum Genders
+    {
+        Man,
+        Woman,
+        None
+    }
+}
